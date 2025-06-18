9c769bc4109bc766e2d2fe8366b3e3421344148e
Sean Foong Jer Tsuen <SBFJTSEA@slake.sssb.com>
2025-06-18 16:44:56 +0800

    Test addition of mike functionalities

--- main.js
+++ main.js
@@ -1,11 +1,67 @@
-import Quill from 'quill';
+// Web Speech API as supported by major browsers
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechGrammarList =
+  window.SpeechGrammarList || window.webkitSpeechGrammarList;
+const SpeechRecognitionEvent =
+  window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
 
-const options = {
-  modules: {
-    toolbar: true,
-  },
-  placeholder: 'Compose an epic...',
-  theme: 'snow'
-};
+const editor = document.getElementById('editor');
 
-const quill = new Quill('#editor', options);
+const quill = new Quill("#editor", {
+  theme: "snow",
+});
+
+// Logic for send button
+const sendButton = document.getElementById('send-button');
+sendButton.addEventListener("click", sendFunction);
+
+// Function for when the send button is clicked
+function sendFunction() {
+  const text = quill.getText()
+  quill.setText("");
+  console.log(text);
+}
+
+// Logic for mike button
+const mikeButton = document.getElementById('mike-button');
+mikeButton.addEventListener("touchstart", mikeStart);
+mikeButton.addEventListener("mousedown", mikeStart);
+mikeButton.addEventListener("touchend", mikeEnd);
+mikeButton.addEventListener("mouseup", mikeEnd);
+
+/**
+ * Setup SpeechRecognition object and configure settings
+ * continous: Whether more than a single result is recognised each time recognition starts
+ * lang: language of recognition
+ * interimResults: Whether interim results are given or if only final results are given
+ * maxAlternatives: Sets the number of potential alternatives
+ */ 
+const recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.lang = 'en-GB';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+
+function mikeStart() {
+  recognition.start();
+  console.log("Begin speech recognition");
+}
+
+function mikeEnd() {
+  recognition.stop()
+  console.log("End speech recognition")
+}
+
+recognition.onresult = function(event) {
+  const speechText = "";
+  for (i = 0; i < event.length; i++) {
+    speechText += event.results[i][0].transcript + " ";
+  }
+
+  quill.updateContents(new Delta()
+    .insert(speechText)
+  );
+  console.log(speechText);
+}
