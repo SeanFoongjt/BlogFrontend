fba1773d62c0d905a8cfe672abf55cd02275b616
Sean Foong Jer Tsuen <SBFJTSEA@slake.sssb.com>
2025-06-19 16:06:20 +0800

    Add all files

---
+++ modules/utilities/getCurrTimeFormatted.js
@@ -0,0 +1,24 @@
+function getCurrTimeFormatted() {
+    const currDate = new Date(Date.now());
+    // Get and parse time
+    
+    var timeHour = currDate.getHours();
+    var timeMinutes = currDate.getMinutes();
+    var timeString = ""
+
+    if (timeMinutes < 10) {
+        timeMinutes = "0" + timeMinutes;
+    }
+
+    if (timeHour < 13) {
+        timeString = timeHour.toString() + "." + timeMinutes + " a.m.";
+    } else if (timeHour == 0) {
+        timeString = "12." + timeMinutes + " a.m."
+    } else {
+        timeString = (timeHour - 12).toString() + "." + timeMinutes + " p.m.";
+    }
+    
+    return timeString;
+}
+
+export {getCurrTimeFormatted}
