dd870e7f8b76a4e95baaac011bf4f54408b7260d
Sean Foong Jer Tsuen <SBFJTSEA@slake.sssb.com>
2025-07-08 17:24:08 +0800

    Begin refactoring createConversation to use Handlebars

--- modules/utilities/createConversation.js
+++ modules/utilities/createConversation.js
@@ -17,63 +17,27 @@ import { encodeText } from "./encodeText.js";
 function createConversation(type, editorHTML, time, text = "", encoding = "Plaintext", 
     replyingTo=false, imagePath ="") {
     if (type == "my-chat") {
+        var contextObj = {
+            text : encodeText(editorHTML, encoding),
+            time : DateTimeFormatting.formatTime(time),
+            encoding: encoding,
+        }
+        var conversationTemplate = fetch("../../templates/my-conversation.html")
+            .then(res => res.text())
+            .then(text => Handlebars.compile(text))
+            .then(template => template(contextObj));
+
         // Initialise chatbox, add dropdown menu
         var chatlog = document.getElementById("chatlog");
         var chatbox = document.createElement("my-chat");
 
-        // Create Dropdown container
-        const dropdown = document.createElement("div");
-        dropdown.setAttribute("slot", "dropdown")
-        dropdown.setAttribute("class", "col dropdown show ms-xl-5 ms-lg-4");
-        chatbox.appendChild(dropdown);
+        var fillChatbox = conversationTemplate.then(item => chatbox.innerHTML = item);
 
-        // Create Dropdown button
-        const dropdownButton = document.createElement("button");
-        dropdownButton.setAttribute("class", "btn btn-secondary dropdown-toggle");
-        dropdownButton.setAttribute("type", "button");
-        dropdownButton.setAttribute("id", "dropdownMenuButton");
-        dropdownButton.setAttribute("data-bs-toggle", "dropdown");
-        dropdownButton.setAttribute("aria-haspopup", "true");
-        dropdownButton.setAttribute("aria-expanded", "false");
-        
-        dropdown.appendChild(dropdownButton);
+        //editButton.addEventListener("click", () => editFunction(chatbox));
 
-        // Create Dropdown menu
-        const dropdownMenu = document.createElement("div");
-        dropdownMenu.setAttribute("class", "dropdown-menu");
-        dropdownMenu.setAttribute("aria-labelledby", "dropdownMenuButton");
-        dropdown.appendChild(dropdownMenu);
+        //deleteButton.addEventListener("click", () => deleteFunction(chatbox));
 
-        // Create Edit button
-        const editButton = document.createElement("a");
-        editButton.setAttribute("class", "dropdown-item");
-        editButton.setAttribute("href", "#");
-        editButton.innerText = "Edit";
-        dropdownMenu.appendChild(editButton);
-        editButton.addEventListener("click", () => editFunction(chatbox));
-
-        // Create Delete button
-        const deleteButton = document.createElement("a");
-        deleteButton.setAttribute("class", "dropdown-item");
-        deleteButton.setAttribute("href", "#");
-        deleteButton.innerText = "Delete";
-        dropdownMenu.appendChild(deleteButton);
-        deleteButton.addEventListener("click", () => deleteFunction(chatbox));
-
-        // Create Forward button
-        const forwardButton = document.createElement("a");
-        forwardButton.setAttribute("class", "dropdown-item");
-        forwardButton.setAttribute("href", "#");
-        forwardButton.innerText = "Forward";
-        dropdownMenu.appendChild(forwardButton);
-
-        // Create Reply button
-        const replyButton = document.createElement("a");
-        replyButton.setAttribute("class", "dropdown-item");
-        replyButton.setAttribute("href", "#");
-        replyButton.innerText = "Reply";
-        dropdownMenu.appendChild(replyButton);
-        replyButton.addEventListener("click", () => replyFunction(chatbox));
+        //replyButton.addEventListener("click", () => replyFunction(chatbox));
 
         // If the chat is replying to another chat, set up a reply banner with text
         // referencing the chat replied
@@ -96,66 +60,49 @@ function createConversation(type, editorHTML, time, text = "", encoding = "Plain
         }
                         
     } else if (type == "other-chat") {
+        var contextObj = {
+            text : encodeText(editorHTML, encoding),
+            time : DateTimeFormatting.formatTime(time),
+            imgsrc: imagePath,
+            encoding: encoding,
+        }
+        var conversationTemplate = fetch("../../templates/other-conversation.html")
+            .then(res => res.text())
+            .then(text => Handlebars.compile(text))
+            .then(template => template(contextObj))
+
         // Initialise chatbox, add dropdown menu
         var chatlog = document.getElementById("chatlog");
         var chatbox = document.createElement("other-chat");
 
-        // Add image
-        var image = document.createElement("img");
-        image.setAttribute("slot", "image");
-        image.setAttribute("class", "mw-100 ")
-        image.src = imagePath;
-        chatbox.appendChild(image);
-
-        // Add in reply icon
-        var replyIcon = document.createElement("i");
-        replyIcon.setAttribute("class", "fa-solid fa-lg fa-reply");
-        replyIcon.setAttribute("slot", "replyIcon");
-        chatbox.appendChild(replyIcon);
-
-        var replyButton = document.createElement("button");
-        replyButton.setAttribute("name", "replyButtonTest");
-        chatbox.appendChild(replyButton);
+        var fillChatbox = conversationTemplate.then(item => chatbox.innerHTML = item);
     }
-    
-    // Create text to go into the slot for actual text of the chat
-    var chatText = document.createElement("div");
-    chatText.setAttribute("slot", "chatText");
-    chatText.setAttribute("name", "text");
 
-    chatText.innerHTML = encodeText(editorHTML, encoding);
-    chatbox.appendChild(chatText);
-
-    // Get time, formatted using module
-    const timeSpan = document.createElement("span");
-    timeSpan.setAttribute("slot", "time");
-    timeSpan.setAttribute("name", "time");
-    timeSpan.innerText = DateTimeFormatting.formatTime(time);
-
-    console.log(chatbox.shadowRoot);
-    chatbox.appendChild(timeSpan);
     chatlog.appendChild(chatbox);
 
     // Can only access shadow root once chatbox appended to chatlog
     if (type == "other-chat") {
-        console.log(chatbox.shadowRoot);
+        
         chatbox
             .shadowRoot
             .querySelector("button[name='replyButton']")
             .addEventListener("click", () => replyFunction(chatbox));
-
         // Center reply button relative to text box
         const textbox = chatbox.shadowRoot.querySelector(".text-box");
-        chatText.setAttribute("data-encoding", encoding);
         const replyButton = chatbox.shadowRoot.querySelector(".btn");
         replyButton.style.marginTop = (textbox.offsetHeight - replyButton.offsetHeight) / 2 + "px";
 
     } else {
         // Center dropdown button relative to text box 
-        const textbox = chatbox.shadowRoot.querySelector(".text-box");
-        chatText.setAttribute("data-encoding", encoding);
-        const padding = (textbox.offsetHeight - chatbox.querySelector(".btn").offsetHeight) / 2;
-        chatbox.querySelector(".dropdown").style.paddingTop = (padding).toString() + "px";
+        Promise.all([fillChatbox])
+            .then(item => {
+                const textbox = chatbox.shadowRoot.querySelector(".text-box");
+                const buttonHeight = chatbox.querySelector(".btn").offsetHeight;
+                const padding = (textbox.offsetHeight - buttonHeight) / 2;
+                chatbox.querySelector(".dropdown").style.paddingTop = (padding).toString() + "px";
+            });
+        //const padding = (textbox.offsetHeight - chatbox.querySelector(".btn").offsetHeight) / 2;
+        //chatbox.querySelector(".dropdown").style.paddingTop = (padding).toString() + "px";
     }
     //console.log("Check 2");
 
