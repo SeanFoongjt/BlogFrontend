d91a64cdb52c264aeaab0c854243e3f6e14a60af
Sean Foong Jer Tsuen <SBFJTSEA@slake.sssb.com>
2025-08-11 15:11:00 +0800

    Add comments for sidebar controller

--- modules/Controller/Sidebar/SideBarController.js
+++ modules/Controller/Sidebar/SideBarController.js
@@ -10,9 +10,21 @@ function SideBarController(parent) {
         closeConversation
     }
 
+    /**
+     * Set sidebar view of the controller, used during initialisation
+     * @param {SidebarView} view 
+     */
     function setView(view) {
         sidebarView = view;
         view.setController(self);
+    }
+
+    /**
+     * Initialise the sidebar, generally just involves rendering via view
+     * @param {Array} conversationList list of conversations to render
+     */
+    function initialise(conversationList) {
+        sidebarView.initialise(conversationList);
     }
 
 
@@ -29,42 +41,68 @@ function SideBarController(parent) {
     conversationSearchText.addEventListener("input", search);
 
 
+    /**
+     * Search the sidebar for a conversation
+     * @param {Event} event input event to detect
+     */
     function search(event) {
+        // As long as the searchbar is not empty, display the search reset button
         if (event.data != null) {
             conversationSearchResetButton.removeAttribute("hidden");
         } else if (conversationSearchText.value == "") {
             conversationSearchResetButton.setAttribute("hidden", "");
         }
 
+        // Ask the model manager to search for conversation models that fit the 
         const conversationList = parent.model.searchConversationsByTitle(
             conversationSearchText.value
         );
+
+        // Get the sidebar information for each conversation that fit the search and render them
         const sidebarList = conversationList.map(conversation => conversation.sidebarInformation());
         sidebarView.render(sidebarList);
     }
 
+    /**
+     * Function to reset searchbar
+     * @param {Event} event event that triggers the function. Should generally be on clicking the
+     * reset button
+     */
     function searchCleanup(event) {
+        // Set searchbar to empty and focus on it
         conversationSearchText.value = "";
         conversationSearchText.focus();
+
+        // hide the reset button
         conversationSearchResetButton.setAttribute("hidden", "true");
+
+        // Render all conversations
         sidebarView.render(parent.model.getSidebarList());
     }
 
 
 
-
+    /**
+     * Close the conversation, i.e. remove the tab from the sidebar
+     * @param {ConversationModel} conversation conversation to be closed
+     */
     function closeConversation(conversation) {
         sidebarView.closeConversation(conversation);
     }
 
-    function initialise(conversationList) {
-        sidebarView.initialise(conversationList);
-    }
-
+    
+    /**
+     * Change the currently active conversation
+     * @param {ConverastionModel} conversation Conversation to be made the new active conversation
+     */
     function changeActive(conversation) {
         sidebarView.changeActive(conversation);
     }
 
+    /**
+     * Update a conversation with new information (title, latest message)
+     * @param {ConverasationModel} conversation conversation to update
+     */
     function updateConversation(conversation) {
         sidebarView.update(conversation);
     }
